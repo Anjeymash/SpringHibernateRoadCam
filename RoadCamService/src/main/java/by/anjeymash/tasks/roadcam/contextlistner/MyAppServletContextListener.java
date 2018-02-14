80f4e860db00cd4899076760e4ae6f0deb1e25fb
Anjeymash <Anjeymash@DESKTOP-489AFBA>
2018-02-14 15:32:30 +0300

    final

--- RoadCamService/src/main/java/by/anjeymash/tasks/roadcam/contextlistner/MyAppServletContextListener.java
+++ RoadCamService/src/main/java/by/anjeymash/tasks/roadcam/contextlistner/MyAppServletContextListener.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletContextListener;
 
 public class MyAppServletContextListener implements ServletContextListener {
 
+	
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
 		System.out.println("ServletContextListener destroyed");
